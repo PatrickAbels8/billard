e8e2aa862bdeff206f529a2b788370973ea3a18e
Samet Katilmis <skatilmi@students.uni-mainz.de>
2020-03-02 20:23:00 +0100

    comment hinzugefügt

--- server/server.py
+++ server/server.py
@@ -3,29 +3,33 @@ from threading import Thread
 import time
 
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 5500
 ADDR = (HOST, PORT)
 BUFSIZ = 512
 
-SERVER  = socket(AF_INET, SOCK_STREAM)
+SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
+# comment
 def game_loop():
     pass
+
 
 def render_board():
     pass
 
+
 def make_shot():
     pass
+
 
 def call_player():
     pass
 
 
-if __name__ == '__main__':
-    SERVER.listen(2) # waiting for 2 clients
-    print('Waiting for 2 players...')
+if __name__ == "__main__":
+    SERVER.listen(2)  # waiting for 2 clients
+    print("Waiting for 2 players...")
 
     SERVER.close()
