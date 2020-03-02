4ce27f3a04ce85dc4af94298c8b967c6401fb038
Samet Katilmis <skatilmi@students.uni-mainz.de>
2020-03-02 20:50:10 +0100

    colors added

--- server/server.py
+++ server/server.py
@@ -11,33 +11,37 @@ BUFSIZ = 512
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
-color_number = {
-    "1": (204, 214, 39),
-    "9": (204, 214, 39),
-    "2": (27, 78, 213),
-    "10": (27, 78, 213),
-    "3": (213, 35, 27),
-    "11": (213, 35, 27),
-    "4": (153, 24, 176),
-    "12": (153, 24, 176),
-    "5": (226, 137, 37),
-    "13": (226, 137, 37),
-    "6": (58, 201, 19),
-    "14": (58, 201, 19),
-    "7": (131, 4, 4),
-    "15": (131, 4, 4),
-    "8": (0, 0, 0),
-}
+color_number = [
+    [1, (204, 214, 39)],
+    [9, (204, 214, 39)],
+    [2, (27, 78, 213)],
+    [10, (27, 78, 213)],
+    [3, (213, 35, 27)],
+    [11, (213, 35, 27)],
+    [4, (153, 24, 176)],
+    [12, (153, 24, 176)],
+    [5, (226, 137, 37)],
+    [13, (226, 137, 37)],
+    [6, (58, 201, 19)],
+    [14, (58, 201, 19)],
+    [7, (131, 4, 4)],
+    [15, (131, 4, 4)],
+    [8, (0, 0, 0)],
+]
 
 
 class Game:
     def __init__(self):
-        self.balls = [Ball(i) for i in range(16)]
+        self.balls = [Ball(i) for i in color_number]
+
+    def make_shot(self):
+        pass
 
 
 class Ball:
     def __init__(self, id):
-        self.id = id
+        self.id, self.color = id
+        # self.color = color_number[str(self.id + 1)]
 
 
 class Player:
@@ -45,27 +49,8 @@ class Player:
         pass
 
 
-def game_loop():
-    pass
-
-
-def render_board():
-    pass
-
-
-def make_shot():
-    pass
-
-
-def call_player():
-    pass
-
-
-"""
-if __name__ == "__main__":
-    SERVER.listen(2)  # waiting for 2 clients
-    print("Waiting for 2 players...")
-
-    SERVER.close()
-"""
-
+G = Game()
+for i in G.balls:
+    print(i.id)  # , i.color)
+for i in color_number:
+    print(color_number[i])
